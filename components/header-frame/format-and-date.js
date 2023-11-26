08ad09ac87a64911a86e4eacbfc66038a28475a7
Mykyta Abramenko <mykyta.abramenko@plus4u.net>
2023-11-26 11:17:06 +0200

    changed link to YouTube translation

--- components/header-frame/format-and-date.js
+++ components/header-frame/format-and-date.js
@@ -11,7 +11,7 @@ function YouTubeVideo() {
     <iframe
       width="100%"
       height="100%"
-      src="https://www.youtube.com/embed/1oq64MAGr2g?si=8lHi9nkW5cA1_tS4"
+      src="https://www.youtube.com/embed/ky48SfLnSdc?si=UZDpaeFpZfqKdHeb"
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
